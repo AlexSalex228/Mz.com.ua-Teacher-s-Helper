700e26d783d50cb6334c325884eb817f55c1a763
Evgen Lesovnikov <lesovnikov@gmail.com>
2018-02-16 18:11:36 +0200

    Update mz-helper.user.js

--- mz-helper.user.js
+++ mz-helper.user.js
@@ -85,7 +85,6 @@ var filllessonsaditional = function(){
                             }
                         });
                 }
-                // schedule%5Blesson_topic%5D=%D0%9D%D0%B0%D0%BB%D0%B0%D1%88%D1%82%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F+%D0%BF%D0%BE%D0%BA%D0%B0%D0%B7%D1%83+%D0%BF%D1%80%D0%B5%D0%B7%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D1%96%D0%B9.&schedule%5Bhometask%5D=g&schedule%5Bschedule_id%5D=262168299&schedule%5B_csrf_token%5D=a4928a4b49ef01e8a4ca22304904d174
             });
         }
         else {
